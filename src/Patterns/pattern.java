742b2b852d7aa4ee8156ef9cd83573cd196d5ede
M V Suneel Reddy <msuneelreddy0@gmail.com>
2025-06-04 17:36:30 +0530

    added pattern2

--- src/Patterns/pattern.java
+++ src/Patterns/pattern.java
@@ -2,12 +2,20 @@ package Patterns;
 
 public class pattern {
     public static void main(String[] args) {
-        pattern1(5);
+        pattern2(5);
     }
 
     private static void pattern1(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    private static void pattern2(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
