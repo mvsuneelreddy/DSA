d44ab714613b5b6a71bf457c981282de03edae5e
M V Suneel Reddy <msuneelreddy0@gmail.com>
2025-06-06 08:22:40 +0530

    pattern4

--- src/Patterns/pattern.java
+++ src/Patterns/pattern.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class pattern {
     public static void main(String[] args) {
-        pattern3(5);
+        pattern4(5);
     }
 
     private static void pattern1(int n) {
@@ -29,5 +29,15 @@ public class pattern {
             System.out.println();
         }
     }
+
+    private static  void pattern4(int n){
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
 }
